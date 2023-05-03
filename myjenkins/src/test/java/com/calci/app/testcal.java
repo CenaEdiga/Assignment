1bad9ffc3a42fe1602e12110bd821aa16881582c
CenaEdiga <132468614+CenaEdiga@users.noreply.github.com>
2023-05-03 21:09:14 +0530

    Add files via upload

---
+++ myjenkins/src/test/java/com/calci/app/testcal.java
@@ -0,0 +1,18 @@
+package com.calci.app;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import com.calci.app.cal;
+public class testcal{
+ 
+    @Test
+    void testAdd()
+    {
+      assertEquals(10,cal.add(5,5));
+    }
+    @Test
+    void testAdd1()
+    {
+      assertEquals(10,cal.add(5,5));
+    }
+
+}
